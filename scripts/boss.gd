750831c5a4c8e3f16c48e2bb11565819321cb8de
leMarlon <2496713@cmontmorency.qc.ca>
2025-12-06 16:38:26 -0500

    idk

---
+++ scripts/boss.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var current_dir = "none"
+
+var speed = 65
+var player_chase = false
+var player = null
+
+
+
+var health = 150
+var player_inattack_zone = false
+
+var can_take_damage = true
+var enemy_alive = true
+var is_hit = false
